bfae846747b900a09bded1943b5a345f942706cd
Elite007 <alijonoveldorbek78@gmail.com>
2025-01-17 21:54:52 +0900

    sef

---
+++ expanding cards/script.js
@@ -0,0 +1,16 @@
+const panels =document.querySelectorAll('.panel')
+
+panels.forEach((panel) =>{
+    panel.addEventListener('click', () =>{
+        panels.forEach(panel =>{
+            panel.classList.remove('active')
+        })
+        panel.classList.add('active')
+    })
+})
+
+// function remoteActiveClasses(){
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
