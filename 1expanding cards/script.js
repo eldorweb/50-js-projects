08651381749dfd61d2458c7eb70a8351598e6a46
Elite007 <alijonoveldorbek78@gmail.com>
2025-03-24 00:50:38 +0900

    sfvse

--- 1expanding cards/script.js
+++ 1expanding cards/script.js
@@ -2,8 +2,8 @@ const panels =document.querySelectorAll('.panel')
 
 panels.forEach((panel) =>{
     panel.addEventListener('click', () =>{
-        panels.forEach(panel =>{
-            panel.classList.remove('active')
+        panels.forEach(pan =>{
+            pan.classList.remove('active')
         })
         panel.classList.add('active')
     })
